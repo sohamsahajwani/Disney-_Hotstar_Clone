c1c8271c83926ea303450f2073ee4da69f00d06a
Code Sandbox <hello@codesandbox.io>
2023-07-25 05:58:00 +0000

    Deployment from Codesandbox.

---
+++ index.js
@@ -0,0 +1,117 @@
+let movies = [
+  {
+    name: "wanda vision",
+    des:
+      "Blends the style of classic sitcoms with the MCU, in which Wanda Maximoff and Vision - two super-powered beings living their ideal suburban lives - begin to suspect that everything is not as it seems.",
+    image: "images/slider 3.PNG"
+  },
+  {
+    name: "falcon and the winter soldier",
+    des:
+      "Following the events of 'Avengers: Endgame,' Sam Wilson/Falcon and Bucky Barnes/Winter Soldier team up in a global adventure that tests their abilities and their patience.",
+    image: "images/slider 2.PNG"
+  },
+  {
+    name: "raya and the last dragon",
+    des:
+      "Raya and the Last Dragon is set in Kumandra, a land where people once lived happily, guarded by magical dragons who brought water, prosperity and peace.",
+    image: "images/slider 4.PNG"
+  },
+  {
+    name: "loki",
+    des:
+      "Loki revolves around the mischievous villain escaping the clutches of The Avengers and getting caught by the Time Variance Authority.",
+    image: "images/slider 1.PNG"
+  },
+  {
+    name: "luca",
+    des:
+      "A young boy experiences an unforgettable seaside summer on the Italian Riviera filled with gelato, pasta and endless scooter rides.",
+    image: "images/slider 5.PNG"
+  }
+];
+
+// Carousel
+const carousel = document.querySelector(".carousel");
+let sliders = [];
+
+let slideIndex = 0;
+
+const createSlide = () => {
+  if (slideIndex >= movies.length) {
+    slideIndex = 0;
+  }
+
+  // Creating DOM element
+  let slide = document.createElement("div");
+  let imgElement = document.createElement("img");
+  let content = document.createElement("div");
+  let h1 = document.createElement("h1");
+  let p = document.createElement("p");
+
+  // Attaching all elements
+  imgElement.appendChild(document.createTextNode(""));
+  h1.appendChild(document.createTextNode(movies[slideIndex].name));
+  p.appendChild(document.createTextNode(movies[slideIndex].des));
+  content.appendChild(h1);
+  content.appendChild(p);
+  slide.appendChild(imgElement);
+  slide.appendChild(content);
+  carousel.appendChild(slide);
+
+  // Setting up image
+  imgElement.src = movies[slideIndex].image;
+  slideIndex++;
+
+  // Setting elements class-name
+  slide.className = "slider";
+  content.className = "slide-content";
+  h1.className = "movie-title";
+  p.className = "movie-des";
+
+  sliders.push(slide);
+
+  if (sliders.length) {
+    sliders[0].style.marginLeft = `calc(-${100 * (sliders.length - 2)}% - ${
+      30 * (sliders.length - 2)
+    }px)`;
+  }
+};
+
+for (let i = 0; i < 5; i++) {
+  createSlide();
+}
+
+setInterval(() => {
+  createSlide();
+}, 3000);
+
+const videoCards = [...document.querySelectorAll(".video-card")];
+
+videoCards.forEach((item) => {
+  item.addEventListener("mouseover", () => {
+    let video = item.children[1];
+    video.play();
+  });
+  item.addEventListener("mouseleave", () => {
+    let video = item.children[1];
+    video.pause();
+  });
+});
+
+let cardContainers = [...document.querySelectorAll(".card-container")];
+let preBtns = [...document.querySelectorAll(".pre-btn")];
+let nxtBtns = [...document.querySelectorAll(".nxt-btn")];
+
+cardContainers.forEach((item, i) => {
+  let containerDimensions = item.getBoundingClientRect();
+  let containerWidth = containerDimensions.width;
+
+  nxtBtns[i].addEventListener("click", () => {
+    item.scrollLeft += containerWidth - 200;
+  });
+
+  preBtns[i].addEventListener("click", () => {
+    item.scrollLeft -= containerWidth + 200;
+  });
+});
